6fb64416b02bc7cf24709e8993410a2a41664c90
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-09-14 16:17:44 +0900

    Cardを整備

---
+++ src/components/Card/Card.style.ts
@@ -0,0 +1,10 @@
+import { StackProps } from "@chakra-ui/react";
+
+export const cardProps: StackProps = {
+  maxW: ["sm", "md", "lg"],
+  boxShadow: "lg",
+  borderWidth: "1px",
+  borderColor: "orange.100",
+  borderRadius: "lg",
+  backgroundColor: "orange.50",
+};
