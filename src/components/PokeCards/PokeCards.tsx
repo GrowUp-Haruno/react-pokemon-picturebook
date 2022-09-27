4b8e24454d6654bbed26a9aec57e139c3e59f121
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-09-27 10:43:07 +0900

    詳細をモーダル表示にする Fixed #24

--- src/components/PokeCards/PokeCards.tsx
+++ src/components/PokeCards/PokeCards.tsx
@@ -1,29 +1,18 @@
-import {
-  Center,
-  Divider,
-  Heading,
-  HStack,
-  Image,
-  SimpleGrid,
-  Spinner,
-  TableContainer,
-  Table,
-  Tbody,
-  Tr,
-  Td,
-  Tag,
-  Text,
-} from "@chakra-ui/react";
+import { Center, Heading, HStack, Image, SimpleGrid, Spinner, Tag, Text } from "@chakra-ui/react";
 import { FC } from "react";
 import { PokeData } from "../App/App.model";
 
 import { Card } from "../Card/Card";
+import { PokeModal } from "../PokeModal/PokeModal";
 import { pokecardsProps, typeColors } from "./PokeCards.style";
+import { usePokeCards } from "./PokeCards.use";
 
 export const PokeCards: FC<{
   pokeDatas: Array<PromiseSettledResult<PokeData>>;
   isLoading: boolean;
 }> = ({ pokeDatas, isLoading }) => {
+  const { modalPokeData, isOpen, handleModalOpen, handleModalClose } = usePokeCards();
+
   if (isLoading)
     return (
       <Center>
@@ -32,58 +21,43 @@ export const PokeCards: FC<{
     );
 
   return (
-    <SimpleGrid {...pokecardsProps}>
-      {pokeDatas.map((pokeData) =>
-        pokeData.status === "fulfilled" ? (
-          <Card key={pokeData.value.name}>
-            {/* 全国図鑑番号 */}
-            <Text alignItems="start">No.{pokeData.value.id}</Text>
-            {/* 画像 */}
-            <Image src={pokeData.value.sprites.front_default} alt={pokeData.value.name} />
-            {/* 名前 */}
-            <Heading as="h2" size="lg">
-              {pokeData.value.name}
-            </Heading>
-            {/* 属性 */}
-            <HStack spacing={4}>
-              {pokeData.value.types.map((type, i) => (
-                <Tag
-                  key={type.type.name}
-                  size="md"
-                  bgColor={typeColors[type.type.name].bgColor}
-                  color={typeColors[type.type.name].color}
-                >
-                  {type.type.name}
-                </Tag>
-              ))}
-            </HStack>
-            <Divider />
-            {/* 詳細 */}
-            <TableContainer overflowX="hidden">
-              <Table variant="simple" size="sm">
-                <Tbody>
-                  <Tr>
-                    <Td>重さ</Td>
-                    <Td isNumeric>{(pokeData.value.weight / 10).toFixed(1)}kg</Td>
-                  </Tr>
-                  <Tr>
-                    <Td>高さ</Td>
-                    <Td isNumeric>{(pokeData.value.height / 10).toFixed(1)}m</Td>
-                  </Tr>
-                  {pokeData.value.abilities.map((ability, index) => (
-                    <Tr key={index}>
-                      <Td>{ability.is_hidden ? "隠れ" : ""}特性</Td>
-                      <Td isNumeric>{ability.ability.name}</Td>
-                    </Tr>
-                  ))}
-                </Tbody>
-              </Table>
-            </TableContainer>
-          </Card>
-        ) : (
-          <></>
-        )
-      )}
-    </SimpleGrid>
+    <>
+      <SimpleGrid {...pokecardsProps}>
+        {pokeDatas.map((pokeData) => {
+          if (pokeData.status === "rejected") return <></>;
+          return (
+            <Card
+              key={pokeData.value.name}
+              onClick={() => {
+                handleModalOpen(pokeData.value);
+              }}
+            >
+              {/* 全国図鑑番号 */}
+              <Text alignItems="start">No.{pokeData.value.id}</Text>
+              {/* 画像 */}
+              <Image src={pokeData.value.sprites.front_default} alt={pokeData.value.name} />
+              {/* 名前 */}
+              <Heading as="h2" size="lg">
+                {pokeData.value.name}
+              </Heading>
+              {/* 属性 */}
+              <HStack spacing={4}>
+                {pokeData.value.types.map((type) => (
+                  <Tag
+                    key={type.type.name}
+                    size="md"
+                    bgColor={typeColors[type.type.name].bgColor}
+                    color={typeColors[type.type.name].color}
+                  >
+                    {type.type.name}
+                  </Tag>
+                ))}
+              </HStack>
+            </Card>
+          );
+        })}
+      </SimpleGrid>
+      <PokeModal isOpen={isOpen} modalPokeData={modalPokeData} handleModalClose={handleModalClose} />
+    </>
   );
 };
